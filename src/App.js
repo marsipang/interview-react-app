5c51c05e91a4b4d64cb757ca4c4623906c295f59
Marissa Pang <pangie490@gmail.com>
2023-02-13 14:41:06 +0100

    updating app classnames

--- src/App.js
+++ src/App.js
@@ -10,12 +10,12 @@ export default function App() {
   const [history, setHistory] = useState([]);
 
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <h1 className="App-title">Cognite Chat App</h1>
+    <div className="app">
+      <header className="app-header">
+        <img src={logo} className="app-logo" alt="logo" />
+        <h1 className="app-title">Cognite Chat App</h1>
       </header>
-      <div className="Body">
+      <div className="body">
         <FriendPanel selectFriend={selectFriend} selectedFriend={selectedFriend} message={message} setMessage={setMessage} history={history} setHistory={setHistory}/>
         <MessagePanel selectedFriend={selectedFriend} setMessage={setMessage} message={message} history={history} setHistory={setHistory}/>
       </div>
